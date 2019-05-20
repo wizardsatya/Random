24e5d7ab1f9b78221f957717de690cb4062b8fdb
wizardsatya <mesatyaprakashsharma@gmail.com>
2019-05-20 08:21:02 -0400

    Add files via upload

---
+++ Random/Random/Program.cs
@@ -0,0 +1,37 @@
+﻿using Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* st
+            int[] arr = { 1, 3, 4, 7, 9 ,13};
+            int a = 4;
+            bool IsTrue = FindDiff.FindTheDiff(arr, a);
+            Console.WriteLine(IsTrue);
+            */
+            /*2nd
+            string BinString = "10110111";
+            string NBinStr = BinString.TrimStart('0');
+            int length = 0;
+            if (NBinStr.Length > 0)
+            {
+                length = FindUniversalString.FindUniversalString(NBinStr);
+            }
+             
+            Console.WriteLine("Intital String : " + BinString);
+            Console.WriteLine("Maximum string: " + length);
+             */
+
+            Console.ReadKey();
+        }
+
+    }
+}
